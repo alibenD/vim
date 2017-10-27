83c3bff7d9813ff334407ca6efd9b49a93023c00
alibenD <aliben.develop@gmail.com>
2017-10-27 13:54:30 +0800

    Adding a templete for ros_node with cpp

--- template/dynamic/full/ros_node.cc.bash
+++ template/dynamic/full/ros_node.cc.bash
@@ -1,31 +1,41 @@
 #!/bin/bash
 #---Automatically Generated from template 'bash' wrote by @aliben---
 # 
-# @name: main.cc.bash
+# @name: ros_node.cc.bash
 # @author: aliben.develop@gmail.com
 # @created_date: 2017-10-23 00:46:17
-# @last_modified_date: NO_LAST_MODIFIED_DATE
+# @last_modified_date: 2017-10-27 13:52:53
 # @description: Generate a template for new main.cc(full-name)
 #---***********************************************---
 
 
 #---Variables
 CREATED_TIME=`date '+%Y-%m-%d %H:%M:%S'`
+CREATED_YEAR=`date '+%Y'`
 
 #---Shell Command
 cat << EOF
 /**
   * @Copyright Dorabot Inc.
-  * @date: $CREATED_TIME
+  * @date: $CREATED_YEAR
   * @author: kaiwen.hu@dorabot.com
   * @brief: TODO
   */
 
+/**-----------------------------------------------
+  * @filename: ${1##*/}
+  * @created_date: $CREATED_TIME
+  * @last_modified_date: NO_LAST_MODIFIED_DATE
+  *-----------------------------------------------*/
+
 //INCLUDE
+#include <ros/ros.h>
 
 //CODE
 
 int main(int argc, char** argv){
+  ros::init(argc, argv, "Node_Name");
+  ros::NodeHandle nh;
 
   return 0;
 }
