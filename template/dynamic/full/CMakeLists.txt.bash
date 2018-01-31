6f99fe503e0f04932e91e963c938890bf905ddd3
alibenD <aliben.develop@gmail.com>
2018-01-31 14:48:19 +0800

    Update template of CMakeLists.txt.bash

--- template/dynamic/full/CMakeLists.txt.bash
+++ template/dynamic/full/CMakeLists.txt.bash
@@ -5,7 +5,7 @@
 # @name: cmakelists.bash
 # @author: aliben.develop@gmail.com
 # @created_date: 2018-01-31 13:47:34
-# @last_modified_date: 2018-01-31 14:20:59
+# @last_modified_date: 2018-01-31 14:46:58
 # @description: TODO
 #---***********************************************---
 
@@ -16,7 +16,7 @@ CREATED_YEAR=`date '+%Y'`
 
 #---Shell Command
 cat << EOF
-#---Automatically Generated from template 'makefile' wrote by @aliben---
+#---Automatically Generated from template 'CMakeList.txt' wrote by @aliben---
 #
 # @Copyright (C) $CREATED_YEAR All rights reserved.
 # @filename: ${1##*/}
@@ -33,12 +33,15 @@ cmake_minimum_required(VERSION 2.8)
 project(NEED_TO_FILL)
 
 # Set Dependence PATH
+  # Example: set ( PACKAGE_DIR PATH )
 
 # Find *Config.cmake
-  # Example find_package(OpenCV REQUIRED)
+  # Example: find_package(OpenCV REQUIRED)
 
 # EXECUTABLE
+  # Example: add_executable( EXEC_NAME SRC_FILE_NAME_LIST )
 
 # TARGET LINK
+  # Example: target_link_libraries( EXEC_NAME LIBPATH ) ...LIB_PATH e.g. \${OPENCV_LIBS}
 
 EOF
