7c3be0c0861f753adac8e48ef159cde2bad848af
alibenD <aliben.develop@gmail.com>
2018-05-31 14:27:03 +0800

    Update Makefile Templete

--- template/dynamic/full/makefile.bash
+++ template/dynamic/full/makefile.bash
@@ -5,7 +5,7 @@
 # @name: makefile.bash
 # @author: aliben.develop@gmail.com
 # @created_date: 2017-10-23 10:37:14
-# @last_modified_date: 2018-01-31 14:19:05
+# @last_modified_date: 2018-05-31 14:25:44
 # @description: Generate a template for a new makefile
 #---***********************************************---
 
@@ -23,20 +23,26 @@ HEAD_POSTFIX=${HEAD_POSTFIX#*.}
 SOURCE_DIR="./src/"
 INCLUDE_DIR="./include/"
 BUILD_DIR="./build/"
+TEST_DIR="./test/"
 OBJECT_DIR=$BUILD_DIR"obj/"
 BINARY_DIR=$BUILD_DIR"bin/"
 
 SOURCE_FILE_LIST=`echo ${SOURCE_DIR}*.${SOURCE_POSTFIX}`
 INCLUDE_FILE_LIST=`echo ${INCLUDE_DIR}*.${HEAD_POSTFIX}`
+TEST_FILE_LIST=`echo ${TEST_DIR}*.${SOURCE_POSTFIX}`
 OBJECT_BUILD_FILE_LIST=`echo ${OBJECT_DIR}*.o`
 BINARY_BUILD_FILE_LIST=`echo ${BINARY_DIR}*`
+TEST_BUILD_FILE_LIST=`echo ${OBJECT_DIR}`
 
 SOURCE_LIST=`echo $(ls $SOURCE_DIR)`
 INCLUDE_LIST=`echo $(ls $INCLUDE_DIR)`
+TEST_LIST=`echo $(ls $TEST_DIR)`
 OBJECT_LIST=`echo $(ls $SOURCE_DIR | sed 's/\.'$SOURCE_POSTFIX'/\.o/g')`
 
 TARGET_OBJECT_LIST=`echo ${SOURCE_LIST}| sed 's/src/build\/obj/g' |sed 's/\.'$SOURCE_POSTFIX'/\.o/g' `
-TARGET_BIN_LIST=`echo ${SOURCE_LIST}| sed 's/src/build\/bin/g' |sed 's/\.'$SOURCE_POSTFIX'//g' `
+TEST_OBJECT_LIST=`echo ${TEST_LIST}| sed 's/src/build\/obj/g' |sed 's/\.'$SOURCE_POSTFIX'/\.o/g' `
+#TARGET_BIN_LIST=`echo ${SOURCE_LIST}| sed 's/src/build\/bin/g' |sed 's/\.'$SOURCE_POSTFIX'//g' `
+TEST_BIN_LIST=`echo ${TEST_LIST}| sed 's/src/build\/bin/g' |sed 's/\.'$SOURCE_POSTFIX'//g' `
 
 #----------------------------------------------------------------------
 
@@ -68,25 +74,31 @@ cat << EOF
     SOURCE_DIR=$SOURCE_DIR
     INCLUDE_DIR=$INCLUDE_DIR
     BUILD_DIR=$BUILD_DIR
+    TEST_DIR=$TEST_DIR
     OBJECT_DIR=$OBJECT_DIR
     BINARY_DIR=$BINARY_DIR
 
   #SOURCE_LIST
     SOURCE_LIST=$SOURCE_LIST
     INCLUDE_LIST=$INCLUDE_LIST
+    TEST_LIST=$TEST_LIST
     OBJECT_LIST=$OBJECT_LIST
 
   #FILE_LIST
     SOURCE_FILE_LIST=$SOURCE_FILE_LIST
     INCLUDE_FILE_LIST=$INCLUDE_FILE_LIST
+    TEST_FILE_LIST=$TEST_FILE_LIST
     OBJECT_BUILD_FILE_LIST=$OBJECT_BUILD_FILE_LIST
+
     BINARY_BUILD_FILE_LIST=`echo ${BINARY_DIR}*`
 
 #----------------------------!!!!!!!-----------------------------
 #---This field should be set while you created your program
   #TARGET_LIST
     TARGET_OBJECT_LIST=$TARGET_OBJECT_LIST
+    TEST_OBJECT_LIST=$TEST_OBJECT_LIST
     TARGET_BIN_LIST=$TARGET_BIN_LIST
+    TEST_BIN_LIST=$TEST_BIN_LIST
 
   #DEPEND_LIST
     DEP=
@@ -100,27 +112,33 @@ cat << EOF
   vpath %.hpp $INCLUDE_DIR
   vpath %.hxx $INCLUDE_DIR
 
-  vpath %.c $SOURCE_DIR
-  vpath %.cc $SOURCE_DIR
-  vpath %.cpp $SOURCE_DIR
-  vpath %.cxx $SOURCE_DIR
+  vpath %.c $SOURCE_DIR:$TEST_DIR
+  vpath %.cc $SOURCE_DIR:$TEST_DIR
+  vpath %.cpp $SOURCE_DIR:$TEST_DIR
+  vpath %.cxx $SOURCE_DIR:$TEST_DIR
 
 #----------------------------------------------------------------------
 #TARGET
 
-all: all_obj \$(TARGET_BIN_LIST)
+all: all_obj \$(TARGET_BIN_LIST) \$(TEST_OBJECT_LIST) \$(TEST_BIN_LIST)
 
 all_obj: \$(TARGET_OBJECT_LIST)
 
 \$(TARGET_OBJECT_LIST): %.o : %.c
 	\$(CC) \$(CCFLAG) -o \$(OBJECT_DIR)\$@ -c \$< \$(INCLUDEFLAG)
 
+\$(TEST_OBJECT_LIST): %.o : %.c
+	\$(CC) \$(CCFLAG) -o \$(OBJECT_DIR)\$@ -c \$< \$(INCLUDEFLAG)
+
 \$(TARGET_BIN_LIST): %: %.o 
 	\$(CC) \$(CCFLAG)  -o \$(BINARY_DIR)\$@ \$(OBJECT_DIR)\$<  \$(OBJECT_DIR)\$(DEP) \$(INCLUDEFLAG)
+
+\$(TEST_BIN_LIST): %: %.o
+  \$(CC) \$(CCFLAG) -o \$(BINARY_DIR)\$@ \$(OBJECT_DIR)\$< \$(OBJECT_DIR)\$(OBJECT_LIST) \$(INCLUDEFLAG)
 
 .PHONY: clean
 
 clean:
-	rm `echo '${OBJECT_BUILD_FILE_LIST}'`
-	rm `echo '${BINARY_BUILD_FILE_LIST}'`
+	rm -rf `echo '${OBJECT_BUILD_FILE_LIST}'`
+	rm -rf `echo '${BINARY_BUILD_FILE_LIST}'`
 EOF
