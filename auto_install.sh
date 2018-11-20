577121bda9cead643e97ed0a113c9bdf36fa034c
alibenD <aliben.develop@gmail.com>
2018-11-20 22:13:57 +0800

    Fixed auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -4,7 +4,7 @@
 # @file: auto_install.sh
 # @author: aliben.develop@gmail.com
 # @created_date: 2018-11-20 21:08:35
-# @last_modified_date: 2018-11-20 21:15:57
+# @last_modified_date: 2018-11-20 22:13:42
 # @brief: TODO
 # @details: TODO
 #---***********************************************---
@@ -16,6 +16,6 @@ CREATED_YEAR=`date '+%Y'`
 
 #---Shell Command
 #brew install vim --with-python --with-ruby --with-perl --with-features=huge --with-lua
-vim +PlugInstall +qall
+vim +PlugInstall +qall &&
 python ./bundle/YouCompleteMe/install.py --clang-completer
 ln -s ./.vimrc8 $HOME/.vimrc
